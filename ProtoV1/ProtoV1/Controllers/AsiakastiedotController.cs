c1aac0c05d7ff25c34a27debd1f93df04e64dc3a
Terhi Fagerlund <terhi.fagerlund@student.careeria.fi>
2020-01-16 14:05:08 +0200

    Asiakascontrollerin, view ja layouttiin lisääminen

---
+++ ProtoV1/ProtoV1/Controllers/AsiakastiedotController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProtoV1.Controllers
+{
+    public class AsiakastiedotController : Controller
+    {
+        // GET: Asiakastiedot
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
