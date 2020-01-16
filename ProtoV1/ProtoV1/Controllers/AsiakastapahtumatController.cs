2175a4d515a44f5310246433253ad55b124eccdb
Sanna-Leena Räsänen <sanna-leena.rasanen@student.careeria.fi>
2020-01-16 14:19:34 +0200

    Asiakastapahtumat controller ja näkymä sekä create näkymä lisätty

---
+++ ProtoV1/ProtoV1/Controllers/AsiakastapahtumatController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProtoV1.Controllers
+{
+    public class AsiakastapahtumatController : Controller
+    {
+        // GET: Asiakastapahtumat
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
