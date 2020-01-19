2eb59072ba5445d97286fdd9868d169640508720
Saaramaria <saaramaria.rassa@student.careeria.fi>
2020-01-19 23:13:22 +0200

    Laskutustiedot-controllerin ja näkymän lisäys

---
+++ ProtoV1/ProtoV1/Controllers/LaskutustiedotController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProtoV1.Controllers
+{
+    public class LaskutustiedotController : Controller
+    {
+        // GET: Laskutustiedot
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
