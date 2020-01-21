2242f3c1c0dc665a3931039d759c055a05e4d34c
Timo Tirkkonen <timo.tirkkonen@student.careeria.fi>
2020-01-21 09:51:08 +0200

    Logins näkymä luotu, appstart-Routeconfig.cs

---
+++ ProtoV1/ProtoV1/Controllers/LoginsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProtoV1.Controllers
+{
+    public class LoginsController : Controller
+    {
+        // GET: Logins
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
