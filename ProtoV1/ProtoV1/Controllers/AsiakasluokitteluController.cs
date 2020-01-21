b9df724f395b5c33e040fa738d4e9ba3ea322aaf
Timo Tirkkonen <timo.tirkkonen@student.careeria.fi>
2020-01-21 12:40:31 +0200

    Asiakasluokittelu: return view tolist

--- ProtoV1/ProtoV1/Controllers/AsiakasluokitteluController.cs
+++ ProtoV1/ProtoV1/Controllers/AsiakasluokitteluController.cs
@@ -3,15 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ProtoV1.Models;
 
 namespace ProtoV1.Controllers
 {
     public class AsiakasluokitteluController : Controller
     {
         // GET: Asiakasluokittelu
+        private SenioritEntities db = new SenioritEntities();
+
         public ActionResult Index()
         {
-            return View();
+            SenioritEntities db = new SenioritEntities();
+            return View(db.Asiakasluokittelu.ToList());
+            //db.Dispose(); << Huom! lisää tämä!
         }
     }
 }
