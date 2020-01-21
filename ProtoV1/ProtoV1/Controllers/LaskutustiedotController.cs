4ddffc4b66ed7d88d37fdcd26fe4c1b777d28136
Saaramaria <saaramaria.rassa@student.careeria.fi>
2020-01-21 12:07:39 +0200

    Lisätty tietoja laskutustiedot-controlleriin

--- ProtoV1/ProtoV1/Controllers/LaskutustiedotController.cs
+++ ProtoV1/ProtoV1/Controllers/LaskutustiedotController.cs
@@ -3,15 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ProtoV1.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace ProtoV1.Controllers
 {
     public class LaskutustiedotController : Controller
     {
-        // GET: Laskutustiedot
+        private SenioritEntities db = new SenioritEntities();
+        // GET: Etusivu
         public ActionResult Index()
+
         {
-            return View();
+            SenioritEntities db = new SenioritEntities();
+            return View(db.Laskutustiedot.ToList());
+            //db.Dispose(); << Huom! lisää tämä!
         }
     }
 }
