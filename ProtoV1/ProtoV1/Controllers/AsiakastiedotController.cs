f9d349cbf21ae4ffa6a219059cb6194563e6e606
Terhi Fagerlund <terhi.fagerlund@student.careeria.fi>
2020-01-21 11:37:20 +0200

    Asiakkaan perustiedot controllerin listaus

--- ProtoV1/ProtoV1/Controllers/AsiakastiedotController.cs
+++ ProtoV1/ProtoV1/Controllers/AsiakastiedotController.cs
@@ -3,15 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
+using ProtoV1.Models;
 
 namespace ProtoV1.Controllers
 {
     public class AsiakastiedotController : Controller
     {
         // GET: Asiakastiedot
+        private SenioritEntities db = new SenioritEntities();
+
         public ActionResult Index()
+
         {
-            return View();
+            SenioritEntities db = new SenioritEntities();
+            return View(db.AsiakkaanPerustiedot.ToList());
+            //db.Dispose(); << Huom! lisää tämä!
         }
     }
 }
