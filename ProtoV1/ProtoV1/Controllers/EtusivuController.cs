61721ce6f9e2c83282db071b6e96cc2f20b81ceb
Timo Tirkkonen <timo.tirkkonen@student.careeria.fi>
2020-01-21 11:14:42 +0200

    Etusivu controllerin db > tolist  view

--- ProtoV1/ProtoV1/Controllers/EtusivuController.cs
+++ ProtoV1/ProtoV1/Controllers/EtusivuController.cs
@@ -3,15 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ProtoV1.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace ProtoV1.Controllers
 {
     public class EtusivuController : Controller
     {
+        private SenioritEntities db = new SenioritEntities();
         // GET: Etusivu
         public ActionResult Index()
+
         {
-            return View();
+            SenioritEntities db = new SenioritEntities();
+            return View(db.AsiakkaanPerustiedot.ToList());
+            //db.Dispose(); << Huom! lisää tämä!
+
+
         }
     }
 }
