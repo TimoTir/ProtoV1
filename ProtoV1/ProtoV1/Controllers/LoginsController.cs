325dc6b792be07728c8ff9b85d0f48d962a88911
Timo Tirkkonen <timo.tirkkonen@student.careeria.fi>
2020-01-21 10:48:36 +0200

    Yhteys LoginsModeliin.

--- ProtoV1/ProtoV1/Controllers/LoginsController.cs
+++ ProtoV1/ProtoV1/Controllers/LoginsController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
+using ProtoV1.Models;
 
 namespace ProtoV1.Controllers
 {
@@ -13,5 +15,40 @@ namespace ProtoV1.Controllers
         {
             return View();
         }
+
+        public ActionResult Käyttäjätunnukset()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Authorize(Logins LoginsModel)
+        {
+            SenioritEntities db = new SenioritEntities();
+
+            var LoggedUser = db.Logins.SingleOrDefault(x => x.Käyttäjätunnus == LoginsModel.Käyttäjätunnus && x.Salasana == LoginsModel.Salasana);
+            if (LoggedUser != null)
+            {
+                ViewBag.LoginMessage = "Successfull login";
+                ViewBag.LoginId = LoggedUser.LoginID;
+                Session["UserName"] = LoggedUser.Käyttäjätunnus;
+                Session["LoginId"] = LoggedUser.LoginID;
+                return RedirectToAction("Index", "Etusivu");
+            }
+            else
+            {
+                ViewBag.LoginMessage = "Login unsuccessfull";
+                ViewBag.LoggedStatus = "Out";
+                LoginsModel.LoginIdErrorMessage = "Tuntematon käyttäjätunnus tai salasana.";
+                return View("Logins", LoginsModel);
+            }
+
+        }
+        public ActionResult LogOut()
+        {
+            Session.Abandon();
+            ViewBag.LoggedStatus = "Out";
+            return RedirectToAction("Index", "Logins"); //Uloskirjautumisen jälkeen pääsivulle
+        }
     }
+
 }
