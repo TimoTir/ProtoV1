5567f6b9e635b428fa8f3cfdbe132de584fd959b
Timo Tirkkonen <timo.tirkkonen@student.careeria.fi>
2020-01-22 15:20:13 +0200

    Ei mitään muutoksia asiakasluokittelunäkymässä

--- ProtoV1/ProtoV1/Models/Asiakasluokittelu.cs
+++ ProtoV1/ProtoV1/Models/Asiakasluokittelu.cs
@@ -11,6 +11,7 @@ namespace ProtoV1.Models
 {
     using System;
     using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
     
     public partial class Asiakasluokittelu
     {
