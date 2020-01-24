1d70736297e896d1ad46b31036862d6a7d61aafa
Sanna-Leena Räsänen <sanna-leena.rasanen@student.careeria.fi>
2020-01-24 10:31:14 +0200

    Uuden asiakastapahtuman lisäys

--- ProtoV1/ProtoV1/Controllers/AsiakastapahtumatController.cs
+++ ProtoV1/ProtoV1/Controllers/AsiakastapahtumatController.cs
@@ -16,8 +16,23 @@ namespace ProtoV1.Controllers
             SenioritEntities db = new SenioritEntities();
             return View(db.Asiakastapahtumat.ToList());
             //db.Dispose(); << Huom! lisää tämä!
-
-
+        }
+        public ActionResult Create()
+        {
+            return View();
+            //db.Dispose(); << Huom! lisää tämä!
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create ([Bind(Include ="Asiakasnumero, Päivämäärä, Yhteydenotto, Kuvaus, Ratkaisu")] Asiakastapahtumat asiakastapahtumat)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Asiakastapahtumat.Add(asiakastapahtumat);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(asiakastapahtumat);
         }
     }
 }
